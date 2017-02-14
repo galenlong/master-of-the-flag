e30bec9e2e3f3fb9c1ac18ec798902b4b355fa4d
nightengalen <garbacchio@gmail.com>
2017-02-14 14:30:51 -0500

    static react page

---
+++ test.jsx
@@ -0,0 +1,149 @@
+// https://facebook.github.io/react/docs/thinking-in-react.html#step-3-identify-the-minimal-but-complete-representation-of-ui-state
+// https://www.npmjs.com/package/react-server-example
+// http://reactjs.cn/react/tips/if-else-in-JSX.html
+// http://reactjs.cn/react/tips/communicate-between-components.html
+// https://www.w3.org/WAI/intro/aria
+
+const Player = {
+	ONE: "p1",
+	TWO: "p2",
+}
+
+var Rank = {
+	SPY: "S",
+	TWO: "2",
+	THREE: "3",
+	FOUR: "4",
+	FIVE: "5",
+	SIX: "6",
+	SEVEN: "7",
+	EIGHT: "8",
+	NINE: "9",
+	TEN: "10",
+	BOMB: "B",
+	FLAG: "F",
+	// EMPTY: 13,
+}
+
+function Piece(rank, player) {
+	this.rank = rank;
+	this.player = player;
+}
+
+function getBoard() {
+	return [
+		[null, new Piece(Rank.THREE, Player.ONE), new Piece(Rank.FIVE, Player.TWO)], 
+		[new Piece(Rank.TWO, Player.TWO), null, null], 
+		[null, null, new Piece(Rank.BOMB, Player.TWO)],
+	];
+}
+
+
+
+class PieceComponent extends React.Component {
+	render() {
+		return (
+			<div className={this.props.player}>
+				{this.props.rank}
+			</div>
+		);
+	}
+}
+
+class Square extends React.Component {
+	render() {
+		return (
+			<td>{this.props.children}</td>
+		);
+	}
+}
+
+class Board extends React.Component {
+	constructor(props) {
+		super(props);
+		this.nbsp = String.fromCharCode(160);
+		// this.board = getBoard();
+	}
+
+	// render() {
+	// 	var self = this;
+	// 	return (
+	// 		<table id="board">
+	// 		<tbody>
+	// 		{this.board.map(function (row, i) {
+	// 			return (
+	// 				<tr key={i}>
+	// 				{row.map(function(piece, j) {
+	// 					var key = i;
+	// 					key += ",";
+	// 					key += j;
+
+	// 					if (piece) {
+	// 						return (
+	// 							<Square key={key}>
+	// 								<PieceComponent rank={piece.rank} 
+	// 									player={piece.player} />
+	// 							</Square>
+	// 						);
+	// 					}
+	// 					return (<Square key={key}>{self.nbsp}</Square>);
+	// 				})}
+	// 				</tr>
+	// 			);
+	// 		})}
+	// 		</tbody>
+	// 		</table>
+	// 	);
+	// }
+
+	render() {
+		return (
+			<table id="board">
+			<tbody>
+			{this.props.board.map((row, i) => 
+				<tr key={i}>
+				{row.map((piece, j) =>
+					(piece) ? 
+						<Square key={i + "," + j}>
+							<PieceComponent rank={piece.rank} 
+								player={piece.player} />
+						</Square> : 
+						<Square key={i + "," + j}>{this.nbsp}</Square>
+				)}
+				</tr>
+			)}
+			</tbody>
+			</table>
+		);
+	}
+}
+
+class Message extends React.Component {
+	render() {
+		return (
+			<div id="message">{this.props.text}</div>
+		);
+	}
+}
+
+class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+		this.board = getBoard();
+	}
+
+	render() {
+		return (
+			<div>
+				<Board board={this.board} />
+				<Message text="hi" />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<Game />,
+	document.getElementById("root")
+);
