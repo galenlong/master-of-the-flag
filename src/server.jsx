f4709c782edbb7208ed4f0827fec675205f202f1
nightengalen <garbacchio@gmail.com>
2017-02-22 15:58:28 -0500

    added favicon

--- src/server.jsx
+++ src/server.jsx
@@ -1,12 +1,16 @@
 var React = require("react");
 var ReactDOMServer = require("react-dom/server");
 
+var favicon = require("serve-favicon");
 var path = require("path");
 var express = require("express");
 var app = express();
 
 var Components = require("./components.js");
 var template = require("./template.js");
+
+// if favicon not showing, quit browser - refresh isn't enough
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 
